3cc193e13956d1ff01c7bd0528cd80db5c4501dc
leimingsheng <leimingsheng@laplacetech.cn>
2018-08-27 17:37:46 +0800

    Initial commit

---
+++ src/main/java/com/dao/Test.java
@@ -0,0 +1,14 @@
+package com.dao;
+
+public class Test {
+    public static void main(String []args){
+        String str = new String("hello");
+        if(str.equals("hello")){
+            System.out.println(str);
+            System.out.println(true);
+        }else {
+            System.out.println(str);
+            System.out.println(false);
+        }
+    }
+}
